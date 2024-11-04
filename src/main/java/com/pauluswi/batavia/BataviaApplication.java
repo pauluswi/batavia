4382d836d92363e9ed435a50ce79d78c169fa7cd
Widodo <slamet.widodo@bankmas.co.id>
2024-11-04 15:06:15 +0700

    Initial

---
+++ src/main/java/com/pauluswi/batavia/BataviaApplication.java
@@ -0,0 +1,13 @@
+package com.pauluswi.batavia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BataviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BataviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauluswi/batavia/BataviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauluswi.batavia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BataviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
